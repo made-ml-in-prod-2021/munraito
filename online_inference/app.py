ddce2dbcf0aee5c5b420ad2de475e75d431bc339
munraito <cvetvitaly@yandex.ru>
2021-05-25 00:01:02 +0300

    remove commented code lines

--- online_inference/app.py
+++ online_inference/app.py
@@ -54,4 +54,4 @@ def predict(request: HeartDiseaseModel):
 if __name__ == "__main__":
     uvicorn.run(
         app, host="0.0.0.0", port=os.getenv("PORT", 8000)
-    )  # config["host"] config["port"]
+    )
